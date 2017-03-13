0fa8718b0df5cf5efffb99385b8b76925c3e72f3
Graham Reeves <graham@grahamreeves.com>
2017-03-13 17:26:34 +0000

    Added USB joystick support

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,9 +1,13 @@
+//  https://github.com/MHeironimus/ArduinoJoystickLibrary/
+#include <Joystick.h>
+
 #include <Adafruit_NeoPixel.h>
 
 #define sizeofarray( a ) ( sizeof(a) / sizeof((a)[0]) )
 
 #define LED_PIN       10
 #define PLAYER_COUNT  4
+#define PAD_COUNT     PLAYER_COUNT
 
 Adafruit_NeoPixel Leds = Adafruit_NeoPixel(12, LED_PIN, NEO_RGB + NEO_KHZ800);
 
@@ -32,6 +36,21 @@ uint32_t PlayerColours[PLAYER_COUNT] =
   Leds.Color( 255,0,255 ),
 };
 
+#define JOYSTICK_BUTTON_COUNT 0
+
+Joystick_ Joystick[PAD_COUNT] = {
+  Joystick_(0x03, JOYSTICK_TYPE_GAMEPAD, JOYSTICK_BUTTON_COUNT, 0, true, false, false, false, false, false, false, false, false, false, false),
+  Joystick_(0x04, JOYSTICK_TYPE_GAMEPAD, JOYSTICK_BUTTON_COUNT, 0, true, false, false, false, false, false, false, false, false, false, false),
+  Joystick_(0x05, JOYSTICK_TYPE_GAMEPAD, JOYSTICK_BUTTON_COUNT, 0, true, false, false, false, false, false, false, false, false, false, false),
+  Joystick_(0x06, JOYSTICK_TYPE_GAMEPAD, JOYSTICK_BUTTON_COUNT, 0, true, false, false, false, false, false, false, false, false, false, false),
+};
+
+#define AXIS_MIN  -100
+#define AXIS_MID  0
+#define AXIS_MAX  100
+
+
+
 void setup() 
 {
   Leds.begin();
@@ -40,6 +59,14 @@ void setup()
 int* AllPins = PlayerJoystickPins[0];
   for ( int i=0;  i<sizeofarray(PlayerJoystickPins);  i++ )
     pinMode(AllPins[i], INPUT);
+
+
+  for ( int p=0;  p<PAD_COUNT;  p++)
+  {
+    auto& Pad = Joystick[p];
+    Pad.begin(false);
+    Pad.setXAxisRange( AXIS_MIN, AXIS_MAX );
+  }
 }
 
 
@@ -69,7 +96,6 @@ int FlashAlternate = 20;
 int RefreshRate = 50;
 void loop() 
 {
-  /*
   for ( int p=0;  p<PLAYER_COUNT; p++ )
   {
     auto PinLeft = PlayerJoystickPins[p][0];
@@ -79,7 +105,8 @@ void loop()
     PlayerDirection[p] = LeftDown ? Direction::Left : RightDown ? Direction::Right : Direction::None;
   }
   
-  for ( int p=0;  p<PLAYER_COUNT; p++ )
+/*  
+ for ( int p=0;  p<PLAYER_COUNT; p++ )
   {
     auto PlayerColour = PlayerColours[p];
     
@@ -94,17 +121,31 @@ void loop()
   for ( int p=0;  p<PLAYER_COUNT; p++ )
   {
     auto PlayerColour = PlayerColours[p];
-    auto PinLeft = PlayerJoystickPins[p][0];
-    auto PinRight = PlayerJoystickPins[p][1];
-    auto LeftDown = digitalRead( PinLeft ) == HIGH;
-    auto RightDown = digitalRead( PinRight ) == HIGH;
+    //auto PinLeft = PlayerJoystickPins[p][0];
+    //auto PinRight = PlayerJoystickPins[p][1];
+    //auto LeftDown = digitalRead( PinLeft ) == HIGH;
+    //auto RightDown = digitalRead( PinRight ) == HIGH;
+    auto LeftDown = PlayerDirection[p] == Direction::Left;
+    auto RightDown = PlayerDirection[p] == Direction::Right;
 
     auto Flash = (FlashCount%FlashAlternate) <= (FlashAlternate/2);
     
     Leds.setPixelColor( PlayerLeds[p][0], LeftDown ? PlayerColour : ColourOff );
     Leds.setPixelColor( PlayerLeds[p][1], Flash ? PlayerColour : ColourOff );
     Leds.setPixelColor( PlayerLeds[p][2], RightDown ? PlayerColour : ColourOff );
-  }
+ 
+  
+    auto& Pad = Joystick[p];
+    int16_t x = AXIS_MID;
+    if ( LeftDown )
+      x = AXIS_MIN;
+    if ( RightDown )
+      x = AXIS_MAX;
+    Pad.setXAxis( x );
+  
+   Pad.sendState();
+ }
+ 
 
   /*
   PlayerOffset++;
